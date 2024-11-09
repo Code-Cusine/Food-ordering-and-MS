cdfcf17f66714fb7e9e2e82ee07c4bf7b5abc434
TejPNaik22 <TejPNaik22@github>
2024-11-09 16:21:42 +0530

    added background wrapper

--- src/App.js
+++ src/App.js
@@ -5,32 +5,40 @@ import Navbar from './components/Navbar/Navbar';
 import Cart from './Pages/Cart';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
-import './App.css';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage/LoginPage';
 import Register from './components/Register/Register';
 import PaymentPage from './components/PaymentPage/PaymentPage';
-import OrderOverlay from './components/Items/OrderOverlay'; // Correct path
+import OrderOverlay from './components/Items/OrderOverlay';
 import Aboutus from './components/Aboutus/Aboutus';
+import PageWrapper from './components/PageWrapper/PageWrapper'; // Import PageWrapper component
+import './App.css';
 
 function App() {
   return (
     <div>
-      
       <BrowserRouter>
         <Navbar />
         <OrderOverlay />
-        <Routes>
-          <Route path='/' element={<Shop />} />
-          <Route path='/drinks' element={<ShopCategory category="Drinks" />} />
-          <Route path='/foods' element={<ShopCategory  category="Food" />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/aboutus' element={<Aboutus />}/>
-          {/*<Route path='/login' element={<LoginPage onClose={() => console.log('Close button clicked')} />} />
-          <Route path='/register' element={<Register onClose={() => console.log('Close button clicked')} />} />*/}
-          <Route path='/payment' element={<PaymentPage />} />
-        </Routes>
+        
+        {/* Wrapping Routes in PageWrapper for custom backgrounds */}
+        <PageWrapper>
+          <Routes>
+            <Route path='/' element={<Shop />} />
+            <Route path='/drinks' element={<ShopCategory category="Drinks" />} />
+            <Route path='/foods' element={<ShopCategory category="Food" />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/aboutus' element={<Aboutus />} />
+            {/* Uncomment these when you need to use them */}
+            {/* 
+            <Route path='/login' element={<LoginPage onClose={() => console.log('Close button clicked')} />} />
+            <Route path='/register' element={<Register onClose={() => console.log('Close button clicked')} />} /> 
+            */}
+            <Route path='/payment' element={<PaymentPage />} />
+          </Routes>
+        </PageWrapper>
+
         <Footer />
       </BrowserRouter>
     </div>
---
+++ src/components/PageWrapper/PageWrapper.js
@@ -0,0 +1,27 @@
+// src/components/PageWrapper/PageWrapper.js
+
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import './PageWrapper.css'; // Correct path for PageWrapper styles
+import '../Items/combined-item-styles.css'; // Corrected path for combined-item-styles.css
+
+const PageWrapper = ({ children }) => {
+  const location = useLocation();
+
+  const getBackgroundClass = () => {
+    switch (location.pathname) {
+      case '/drinks':
+        return 'background-drinks';
+      case '/foods':
+        return 'background-foods';
+      case '/aboutus':
+        return 'background-aboutus';
+      default:
+        return 'background-default';
+    }
+  };
+
+  return <div className={getBackgroundClass()}>{children}</div>;
+};
+
+export default PageWrapper;
